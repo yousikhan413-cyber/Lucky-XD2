4009043ee64fec3f20f299ed735b55f2d807fe24
yousikhan413-cyber <yousikhan413@gmail.com>
2025-07-25 10:12:01 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "lucky~3I0FzSSL#oS7xy_x8t7r7nEGzO9WMKD7XE-fdkiamdXxCHMgchCk",
 // add your Session Id make sure it starts with lucky~
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
@@ -68,7 +68,7 @@ LINK_WHITELIST: "youtube.com,github.com",
           DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
 
-          OWNER_NUMBER: process.env.OWNER_NUMBER || "",
+          OWNER_NUMBER: process.env.OWNER_NUMBER || "962794890589",
 // add your bot owner number
 
 OWNER_NAME: process.env.OWNER_NAME || "",
